da2ed7df6383e0cc95534a1cda171b9eb37e6ddb
Nagy-Zsugya Martin <98815314+Etereke@users.noreply.github.com>
2024-02-01 08:52:42 +0100

    Solve exercises 3-7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removables) {
+    const result = [];
+    for (let i = 0; i < array.length; i++) {
+        if (!removables.includes(array[i])) {
+            result.push(array[i]);
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(first, last) {
+    if (typeof first !== "number" || typeof last !== "number") {
+        return "ERROR";
+    }
+    let sum = 0;
+    if (first > last) {
+        const temp = first;
+        first = last;
+        last = temp;
+    }
+    for (let i = first; i <=last; i++) {
+        sum += i;
+    }
+    return sum > 0 ? sum : "ERROR";
 };
 
 // Do not edit below this line

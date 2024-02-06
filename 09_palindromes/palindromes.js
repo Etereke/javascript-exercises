8c9e2d6552d44ad515f60e5de1bdbcad6290bddb
Nagy-Zsugya Martin <98815314+Etereke@users.noreply.github.com>
2024-02-06 18:05:39 +0100

    Finish all exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let strLower = str.toLowerCase().trim();
+    let strLetters = '';
+    for (let i = 0; i < strLower.length; i++) {
+        if(('a' <= strLower[i] && strLower[i] <= 'z') || ('0' <= strLower[i] && strLower[i] <= '9')) {
+            strLetters += strLower[i];
+        }
+    }
+    
+    return strLetters === strLetters.split('').reverse().join('');
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.sort((a, b) => ((b.yearOfDeath || new Date().getFullYear()) - b.yearOfBirth) - ((a.yearOfDeath || new Date().getFullYear()) - a.yearOfBirth))[0];
 };
-
+const people = [
+      {
+        name: "Carly",
+        yearOfBirth: 1942,
+        yearOfDeath: 1970,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      },
+    ]
 // Do not edit below this line
 module.exports = findTheOldest;
